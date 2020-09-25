30f387952adfd105c6b07ff282dc5c65cc87faf4
Johan Perez <nasa_science@hotmail.com>
2020-09-25 20:38:02 +0200

    image fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,11 +8,11 @@ import { IFutureFlight } from '../../interfaces/future-flight'
 export class HomeComponent implements OnInit {
 
   aircrafts: string[] = ['A-PLN1', 'A-PLN2', 'A-PLN3', 'A-PLN4']
-
+  date : Date = new Date()
   constructor() { }
 
   ngOnInit(): void {
-
+   // this.today = 25
   }
 
   filterAircraft(ircraft: string){
